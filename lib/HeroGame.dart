498b34834dbdec7891d105d413bb6a9ed799cd49
justacoder30 <dangquang0129@gmail.com>
2025-05-07 20:11:21 +0700

    test

---
+++ lib/HeroGame.dart
@@ -0,0 +1,5 @@
+import 'package:flame/game.dart';
+
+class HeroGame extends FlameGame {
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:game_project/HeroGame.dart';
+
+void main() {
+  HeroGame game = HeroGame();
+
+  runApp(GameWidget(game: game));
+}
